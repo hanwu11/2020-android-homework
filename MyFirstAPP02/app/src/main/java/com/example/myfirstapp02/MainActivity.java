7324b66625ba8af7aa044dd8127d6b6fcd56f85d
hanwu11 <2822234733@qq.com>
2020-10-24 19:55:09 +0800

    22020智能手机

---
+++ MyFirstAPP02/app/src/main/java/com/example/myfirstapp02/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myfirstapp02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marquee);
+
+    }
+}
